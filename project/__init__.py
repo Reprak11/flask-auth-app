36da7dbd996020796860f0bd96f1ae284fbafec1
Reprak11 <reynaldo.armenta@cetys.edu.mx>
2021-02-24 19:43:38 -0800

    First commit Heroku

--- project/__init__.py
+++ project/__init__.py
@@ -5,27 +5,25 @@ from flask_login import LoginManager
 #Init SQLAlchemy
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "comunidad-develop-latino"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "comunidad-develop-latino"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-    db.init_app(app)
+db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
-    from .models import User
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+from .models import User
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
 
-    return app
 
 
